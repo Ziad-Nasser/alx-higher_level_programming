29345c6c6ccf8cc474d9f7daaaaa5cd5fd7aab5b
Ziad-Nasser <ziadnasser177@gmail.com>
2023-07-04 01:35:22 +0300

    Adas

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -4,10 +4,15 @@
 
 class Rectangle:
     """Rectangle"""
+
+    number_of_instances = 0
+    print_symbol = "#"
+
     def __init__(self, width=0, height=0):
-        """Initializes the rectangle"""
+        """rectangle"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -62,3 +67,4 @@ class Rectangle:
     def __del__(self):
         """dectractor"""
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -4,10 +4,15 @@
 
 class Rectangle:
     """Rectangle"""
+
+    number_of_instances = 0
+    print_symbol = "#"
+
     def __init__(self, width=0, height=0):
-        """Initializes the rectangle"""
+        """rectangle"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -47,6 +52,22 @@ class Rectangle:
             return 0
         return (self.__width * 2) + (self.__height * 2)
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """biggest rectangle"""
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """returns a new Rectangle instance that is a square w/ h==w==size"""
+        return cls(size, size)
+
     def __str__(self):
         """string representation of the rectangle"""
         string = ""
@@ -62,3 +83,4 @@ class Rectangle:
     def __del__(self):
         """dectractor"""
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
