bca5b27dda02b628132e78cf3ba7d6342d1bb0cb
Ziad-Nasser <ziadnasser177@gmail.com>
2023-07-11 20:11:08 +0300

    fsafasf

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""to_json_string function"""
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object"""
+    import json
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""load_from_json_file"""
+
+
+def load_from_json_file(filename):
+    """creates an Object from a “JSON file“"""
+    import json
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
